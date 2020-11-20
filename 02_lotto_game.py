66a8d88804c04394fe20b05e4e74b000148b05ab
Piotr Michna <pm@piotrmichna.pl>
2020-11-20 20:52:57 +0100

    definition of the number randomizeation function

---
+++ 02_lotto_game.py
@@ -0,0 +1,17 @@
+from random import randint
+
+
+def generate_lotto_numbers():
+    lotto_numbers = []
+    while len(lotto_numbers) < 6:
+        number = randint(1, 49)
+        is_unique = True
+        for i in lotto_numbers:
+            if i == number:
+                is_unique = False
+                break
+
+        if is_unique:
+            lotto_numbers.append(number)
+
+
